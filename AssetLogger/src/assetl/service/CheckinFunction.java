7e2249b6264b7639ae7d00cbfd92f2514d279f4e
devo149 <devo149@ad238382-2587-11df-bd31-99dc06631df6>
2010-04-02 02:03:47 +0000

    Removed support for checking in multiple assets at once. Talked to Steve, we need to program slick but not that slick. We need to slow down checkin process to be sure we have the power cord and that laptop matches the bag number. Not to mention it occurs to me that we would want to enforce scaning the bar code every time.

--- AssetLogger/src/assetl/service/CheckinFunction.java
+++ AssetLogger/src/assetl/service/CheckinFunction.java
@@ -1,10 +1,7 @@
 package assetl.service;
 
 import java.util.Date;
-import java.util.Collection;
-import java.util.ArrayList;
 import assetl.system.DataPacket;
-import assetl.system.AssetCollectionPacket;
 import assetl.system.StringPacket;
 import assetl.system.Asset;
 import assetl.system.Checkout;
@@ -20,7 +17,7 @@ public class CheckinFunction
    /**
     * The specific DataPacket needed for this function to operate
     */
-   private DataPacket mData;
+   private StringPacket mData;
 
    /**
     * Sets the DataPacket for a Checkin
@@ -29,7 +26,7 @@ public class CheckinFunction
     */
    public void setPacket(DataPacket pPacket)
    {
-      mData = pPacket;
+      mData = (StringPacket) pPacket;
    }
 
    /**
@@ -43,31 +40,6 @@ public class CheckinFunction
    }
 
    /**
-    * Gets from the DataPacket the assets that need to be checked in
-    *
-    * @return The collection of assests that need to be checked in
-    */
-   public Collection<Asset> getAssets()
-   {
-      ArrayList<Asset> checkins = new ArrayList<Asset>();
-
-      //Handle different DataPackets
-      checkins = new ArrayList<Asset>();
-      if (StringPacket.class.isAssignableFrom(mData.getClass()))
-      {
-         StringPacket strPack = (StringPacket) mData;
-         checkins.add(mModel.getAsset(strPack.getString()));
-      }
-      else
-      {
-         AssetCollectionPacket colPack = (AssetCollectionPacket) mData;
-         checkins = (ArrayList<Asset>) colPack.getAssets();
-      }
-
-      return checkins;
-   }
-
-   /**
     * Checks in an asset: Retrieves the outstanding Checkout object for this
     * asset and sets today's date as the returned date.
     */
@@ -77,18 +49,32 @@ public class CheckinFunction
       // Get the checkout and set the returned date to today's date
       //
 
-      for (Asset asset : getAssets())
+      Checkout outstanding = null;
+      Asset asset = mModel.getAsset(mData.getString());
+
+      if (asset != null)
       {
-         Checkout outstanding = mModel.getCheckout(asset);
+         outstanding = mModel.getCheckout(asset);
+         System.err.println("asset found");
+
          if (outstanding != null)
          {
+            // TODO: prompt if the power cord is missing
+            // show the barcode and ask for number on bag, if
+            // different don't check it in
+
             outstanding.setReturnedDate(new Date());
             mModel.setCheckout(outstanding);
+            System.err.println("works");
          }
          else
          {
             System.err.println("Item not out.");
          }
+      }
+      else
+      {
+         System.err.println("Not an asset");
       }
    }
 }

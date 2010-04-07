07f1f1d24ca6465f92e58d915ad34f4f12ed94ca
devo149 <devo149@ad238382-2587-11df-bd31-99dc06631df6>
2010-04-07 02:00:26 +0000

    Edited javadocs.

--- trunk/AssetLogger/src/assetl/service/LoadFinderFunction.java
+++ trunk/AssetLogger/src/assetl/service/LoadFinderFunction.java
@@ -4,7 +4,7 @@ import assetl.system.DataPacket;
 import assetl.system.StringPacket;
 
 /**
- * Changes to the FindAsset view and enables either
+ * Changes to the FindAsset view to either
  * a schedule funtion or a checkout function.
  * 
  * @author Devin Doman
@@ -12,18 +12,34 @@ import assetl.system.StringPacket;
 public abstract class LoadFinderFunction
    extends Function
 {
+   /**
+    * Holds the String to send to the next view
+    */
    StringPacket mData;
 
+   /**
+    * Sets the DataPacket for this function
+    *
+    * @param pPacket The DataPacket
+    */
    public void setPacket(DataPacket pPacket)
    {
       mData = (StringPacket) pPacket;
    }
 
+   /**
+    * Returns the DataPacket
+    *
+    * @return The DataPacket
+    */
    public DataPacket getPacket()
    {
       return mData;
    }
 
+   /**
+    * Changes to the view to find assets, sending the DataPacket
+    */
    public void performFunction()
    {
       mControl.changeView("FindAsset");
